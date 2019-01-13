b725a4ec5d416b5a042bd17f8e3b2d2473e5125a
Frank Karunaratna <frank@modiface.com>
2019-01-13 17:24:06 -0500

    add mysql pool connection and dot env file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var mysql = require('mysql');
+require('dotenv').config();
 var router = express.Router();
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : process.env.DB_HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PASS,
+  database        : process.env.DB
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  pool.query('SELECT * from products', function (error, results, fields) {
+    res.send(results);
+  });
 });
 
 module.exports = router;
